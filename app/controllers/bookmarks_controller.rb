bde0a6a565712ff3afd2ccb0785c743eb81d0497
zukam fodouop <zukam.fp@outlook.com>
2024-02-22 17:03:39 +0100

    Watch list rails maj

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -13,8 +13,11 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
     @bookmark.list = @list
-    @bookmark.save
-    redirect_to list_path(@list)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -28,6 +31,6 @@ class BookmarksController < ApplicationController
   end
 
   def bookmark_params
-    params.require(:bookmark).permit(:movie_id, :list, :comment)
+    params.require(:bookmark).permit(:movie_id, :comment)
   end
 end
